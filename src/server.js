c8af86df06da0bff7c23d7676bc43b401d5fd8fc
BenCraig <benjamin.d.craig@gmail.com>
2016-10-20 09:47:51 -0400

    - switched some server stuff to ES6 - shows a view now

--- src/server.js
+++ src/server.js
@@ -1,19 +1,37 @@
-var express = require('express');
-var app = express();
-var url = require('url');
-var bodyParser = require('body-parser');
-var http = require('http').Server(app);
+
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+const app = express();
 
 function start() {
 
+	app.set('port', (process.env.PORT || 80));
+	app.use(express.static(__dirname + '/../public'));
+	app.use(bodyParser.json());
 
-	app.set('views', path.join(__dirname, 'views'));
+	app.set('views', __dirname + '/../views');
 	app.set('view engine', 'pug');
 
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 
 	app.use(express.static(path.join(__dirname, 'public')));
+
+	app.use((req, res, next) => {
+	    res.setHeader('Access-Control-Allow-Origin', '*');
+	    res.setHeader('Cache-Control', 'no-cache');
+	    next();
+	});
+
+	app.get('/', (req, res) => {
+	    res.render('index');
+	});
+
+	app.listen(app.get('port'), () => {
+	    console.log('Server running on localhost:' + app.get('port') + '/');
+	});
 
 }
 
